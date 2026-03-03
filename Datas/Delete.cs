92b01bf9acc2b6d58ed5e6e737ee64e2abd4fc66
FodorRobert <fodorr@kkszki.hu>
2026-03-03 21:34:56 +0100

    Tried doing the delete part but failed to do so

--- Datas/Delete.cs
+++ Datas/Delete.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryGUI.Models;
+using Org.BouncyCastle.Asn1.X509;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,28 +8,70 @@ using System.Threading.Tasks;
 
 namespace LibraryGUI.Datas
 {
-    internal class Delete
+    internal class Create
     {
-        public object DeleteAuthor(int id)
+        LibraryResults libraryResults = new LibraryResults();
+        public LibraryResults DeleteAuthor(string name)
         {
-            LibraryResults results = new LibraryResults();
             using (var context = new librarydbContext())
             {
-                var author = context.Authors.Find(id);
-                if (author != null)
+                var author = new Authors
                 {
-                    context.Authors.Remove(author);
-                    context.SaveChanges();
-                    results.Message = "Sikeres törlés!";
-                    results.Result = author;
-                }
-                else
-                {
-                    results.Message = "Sikertelen törlés!";
-                    results.Result = author;
-                }
-                return results;
+                    AuthorName = name
+                };
+
+                context.Authors.Remove(author);
+                context.SaveChanges();
+                libraryResults.Message = "Sikeres szerző törlés";
+                libraryResults.Result = author.AuthorName;
+
+
+                return libraryResults;
             }
+
+        }
+
+        public LibraryResults DeleteCategory(string categoryName)
+        {
+            using (var context = new librarydbContext())
+            {
+                var category = new Categories
+                {
+                    CategoryName = categoryName
+                };
+
+                context.Categories.Remove(category);
+                context.SaveChanges();
+                libraryResults.Message = "Sikeres kategória törlés";
+                libraryResults.Result = category.CategoryName;
+
+
+                return libraryResults;
+            }
+
+        }
+
+        public LibraryResults DeleteBooks(string title, DateTime date, int authId, int categoryId)
+        {
+            using (var context = new librarydbContext())
+            {
+                var book = new Books
+                {
+                    Title = title,
+                    PublishDate = date,
+                    AuthorId = authId,
+                    CategoryId = categoryId
+                };
+
+                context.Books.Remove(book);
+                context.SaveChanges();
+                libraryResults.Message = "Sikeres könyv törlés";
+                libraryResults.Result = book;
+
+
+                return libraryResults;
+            }
+
         }
     }
 }
